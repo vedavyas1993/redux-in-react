e7ee983085fad025038a2ee7ac682e535c60c7d5
vedavyas1993 <vedavyas1993@gmail.com>
2022-07-27 01:24:46 +0530

    basic redux-toolkit setup complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -6,22 +7,17 @@ const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
   const toggleCounterHandler = () => {
-    dispatch({
-      type: "toggle",
-    });
+    dispatch(counterActions.toggleCounter());
   };
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const increaseHandler = () => {
-    dispatch({
-      type: "increase",
-      amount: 5,
-    });
+    dispatch(counterActions.increase(10));
   };
   return (
     <main className={classes.counter}>
